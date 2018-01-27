35804025dadd769fc821b54b9650cca8f3c01609
osaichuk <oleksandr.saichuk@gmail.com>
2018-01-27 16:57:44 +0200

    add php_basics_tasks gitignore

---
+++ php_basics_tasks/26.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Объявите константу DAYS_COUNT равную 7
+ * и константу MONTH_COUNT равную 12 двумя
+ * разными способами объявления констант.
+ */
+
+define('DAYS_COUNT', 7);
+const MONTH_COUNT = '12';
+
+echo DAYS_COUNT;
+echo '<br />';
+echo MONTH_COUNT;
